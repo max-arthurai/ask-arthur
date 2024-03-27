17150a72a58cf663103765972d3bedeb30b92792
Max Cembalest <maxcembalest@Maxs-MBP-2.local.meter>
2024-03-27 14:16:01 -0400

    updated benchmark

--- bench_eval.py
+++ bench_eval.py
@@ -1,8 +1,11 @@
 from arthur_bench.run.testsuite import TestSuite
-from arthur_bench.scoring import Scorer
+from arthur_bench.scoring import Scorer, QAQualityCorrectness
 import pandas as pd
 from sentence_transformers import SentenceTransformer, util
 from typing import List, Optional
+
+from langchain.chat_models import ChatOpenAI
+from langchain_anthropic import ChatAnthropic
 
 df = pd.read_csv("arthur_benchmark_2024-03-26.csv", index_col=0)
 print(df.head())
@@ -38,17 +41,27 @@ class EmbeddingSimilarity(Scorer):
         return outputs
 
 
-suite = TestSuite(
-    'scope-bench-nomic-similarity',
-    EmbeddingSimilarity(),
-    input_text_list=df.question.tolist(),
-    reference_output_list=df.golden_answer.tolist()
+qa_scorer = QAQualityCorrectness(
+    # llm=ChatOpenAI(model="gpt-4-0125-preview")
+    llm=ChatAnthropic(model="claude-3-opus-20240229")
 )
 
-run_names = [c for c in df.columns if c not in ["question", "golden_answer"]]
+
+suite = TestSuite(
+    # 'scope-bench-nomic-similarity',
+    # EmbeddingSimilarity(),
+    'scope-bench-qa-correctness-opus',
+    qa_scorer,
+    input_text_list=df.question.tolist(),
+    # reference_output_list=df.golden_answer.tolist()
+)
+
+run_names = [c for c in df.columns if c not in ["question", "golden_context", "golden_answer"]]
 for run_name in run_names:
+    print(run_name)
     suite.run(
         run_name.replace("/", "_"),
         candidate_data=df,
-        candidate_column=run_name
+        candidate_column=run_name,
+        context_column="golden_context"
     )
