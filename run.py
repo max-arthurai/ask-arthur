a8c97202d128e35c12244596e74aef731fbd1100
Max Cembalest <maxcembalest@Maxs-MBP-2.local.meter>
2024-04-04 18:36:39 -0400

    multi turn chat ollama version

--- run.py
+++ run.py
@@ -2,6 +2,7 @@ import argparse
 from versions.langchain_version import run as run_langchain
 from versions.dspy_version import run as run_dspy
 from versions.ollama_version import run as run_ollama
+from versions.ollama_version import chat as chat_ollama
 
 
 if __name__ == "__main__":
@@ -12,6 +13,7 @@ if __name__ == "__main__":
     parser.add_argument('--llm', default='gpt-3.5-turbo-0125')
     parser.add_argument('--prompt', default='Give it to me short and sweet - why should I buy Arthur?')
     parser.add_argument('--framework', default='langchain')
+    parser.add_argument('--mode', default='prompt')
     parser.add_argument('--verbose', action='store_true')
     args = parser.parse_args()
     if args.framework == 'langchain':
@@ -19,6 +21,9 @@ if __name__ == "__main__":
     elif args.framework == 'dspy':
         run_dspy(args.prompt, args.llm, args.embedding)
     elif args.framework == 'ollama':
-        run_ollama(args.prompt)
+        if args.mode == 'chat':
+            chat_ollama()
+        else:
+            run_ollama(args.prompt)
     else:
         raise ValueError(f"unrecognized framework: {str(args)}")
--- versions/ollama_version.py
+++ versions/ollama_version.py
@@ -5,25 +5,6 @@ from transformers import AutoTokenizer
 
 tokenizer = AutoTokenizer.from_pretrained("CohereForAI/c4ai-command-r-v01")
 
-tools = [
-  {
-    "name": "internet_search",
-    "description": "Returns a list of relevant document snippets for a textual query retrieved from the internet",
-    "parameter_definitions": {
-      "query": {
-        "description": "Query to search the internet with",
-        "type": 'str',
-        "required": True
-      }
-    }
-  },
-  {
-    'name': "directly_answer",
-    "description": "Calls a standard (un-augmented) AI chatbot to generate a response given the conversation history",
-    'parameter_definitions': {}
-  }
-]
-
 chroma_client = chromadb.PersistentClient("data/chroma/chroma")
 arthur_index = chroma_client.get_collection("arthur_index")
 
@@ -35,7 +16,7 @@ def generate(
     stop_phrase: str = "Grounded answer: ",
     temperature: float = 0.0,
     verbose: bool = True,
-):
+) -> str:
     """Responds with the command-r model via the Ollama local API
 
     Args:
@@ -61,8 +42,7 @@ def generate(
     for chunk in generator:
         chunk_text = chunk["response"]
         response += chunk_text
-        if verbose:
-            print(chunk_text, end="", flush=True)
+        print(chunk_text, end="", flush=True)
         if stop_phrase in response:
             response = response.split(stop_phrase)[0]
             break
@@ -71,7 +51,7 @@ def generate(
     return response
 
 
-def get_search_documents(s: str, k: int = 3):
+def get_search_documents(s: str, k: int = 3) -> list[dict[str, str]]:
     """Uses the Nomic embedding model via the Ollama local API to search for relevant documents from arthur_index
 
     Args:
@@ -95,47 +75,132 @@ def get_search_documents(s: str, k: int = 3):
     return documents
 
 
-def command_r_respond(conversation: list[dict[str:str]], verbose=True):
-    """Responds to a conversation"""
-    tool_prompt = tokenizer.apply_tool_use_template(
+def command_r_tool_response(
+    conversation: list[dict[str, str]],
+    verbose: bool = True
+) -> dict:
+    """Calls command-r with the tool-use prompt template
+
+    Args:
+        conversation: list[dict[str,str]]
+        verbose: bool
+    Returns:
+        tool_response: dict["tool_name" : str, "parameters" : dict[str , str]]
+    """
+    tools = [{
+        "name": "internet_search",
+        "description": "Returns a list of relevant document snippets for a textual query retrieved from the internet",
+        "parameter_definitions": {
+            "query": {
+                "description": "Query to search the internet with",
+                "type": 'str',
+                "required": True
+            }
+        }
+    }, {
+        "name": "directly_answer",
+        "description": "Calls a standard (un-augmented) AI chatbot to generate a response given the conversation \
+        history",
+        "parameter_definitions": {}
+    }]
+    tools_prompt = tokenizer.apply_tool_use_template(
         conversation,
         tools=tools,
         tokenize=False,
         add_generation_prompt=True,
     )
-    if verbose: print("choosing tool")
-    tool_response = generate(tool_prompt, verbose=False)
-    tool_response = json.loads(tool_response.split("```json")[1].split("```")[0])
-    tool_name = tool_response[0]["tool_name"]
-    if tool_name == "directly_answer":
-        response_prompt = tokenizer.apply_chat_template(
-            conversation,
-            tokenize=False,
-            add_generation_prompt=True,
-        )
-    elif tool_name == "internet_search":
-        query = tool_response[0]["parameters"]["query"]
-        documents = get_search_documents(query)
-        response_prompt = tokenizer.apply_grounded_generation_template(
-            conversation,
-            documents=documents,
-            citation_mode="accurate",
-            tokenize=False,
-            add_generation_prompt=True,
-        )
-    else:
-        raise ValueError("bad tool name :/")
-    response = generate(response_prompt, verbose=True)
+    if verbose:
+        print("choosing tool")
+    tools_response = generate(tools_prompt, verbose=verbose)
+    tools_response = json.loads(tools_response.split("```json")[1].split("```")[0])
+    return tools_response
+
+
+def command_r_chat_response(
+    conversation: list[dict[str, str]],
+    verbose=True
+) -> str:
+    """Calls command-r with the chat prompt template
+
+    Args:
+        conversation: list[dict[str:str]]
+        verbose: bool
+    Returns:
+        response: str
+    """
+    response_prompt = tokenizer.apply_chat_template(
+        conversation,
+        tokenize=False,
+        add_generation_prompt=True,
+    )
+    response = generate(response_prompt, verbose=verbose)
     return response
+
+
+def command_r_grounded_response(
+    conversation: list[dict[str, str]],
+    documents: list[dict[str, str]],
+    verbose=True
+) -> str:
+    """Calls command-r with the tool-use prompt template
+
+    Args:
+        conversation: list[dict[str:str]]
+        documents: list[dict[str:str]]
+        verbose: bool
+    Returns:
+        tool_response: dict["tool_name" : str, "parameters" : dict[str : str]]
+    """
+    response_prompt = tokenizer.apply_grounded_generation_template(
+        conversation,
+        documents=documents,
+        citation_mode="accurate",
+        tokenize=False,
+        add_generation_prompt=True,
+    )
+    return generate(response_prompt, verbose=verbose)
+
+
+def command_r_respond(conversation: list[dict[str:str]], verbose=False) -> str:
+    """Responds to a conversation
+
+    Args:
+        conversation: list[dict[str:str]]
+        verbose: bool
+    Returns:
+        response: str
+    """
+    tools_response = command_r_tool_response(conversation, verbose=verbose)
+    if not any(x["tool_name"] == "internet_search" for x in tools_response):
+        return command_r_chat_response(conversation, verbose=verbose)
+    else:
+        documents = []
+        for x in tools_response:
+            if x["tool_name"] == "internet_search":
+                documents.extend(get_search_documents(x["parameters"]["query"]))
+        return command_r_grounded_response(conversation, documents, verbose=verbose)
 
 
 def run(prompt: str):
     system_prompt = """The purpose of your job is to write search queries specifically for the AI company Arthur AI.
 Arthur AI is the AI performance company. Arthur offers four products (with integrations between the four):
-Arthur Scope is an API-first enterprise suite of tools for robust, efficient, and scalable metrics, monitoring, dashboards, and alerting.
-Arthur Shield is an API for detecting problems in LLM pipelines - hallucinations, prompt injections, leaks of personally identifiable or otherwise sensitive data, and toxic/malicious language and intent.
-Arthur Bench is an open source python package with built in UI for evaluating and comparing the components of LLM pipelines - different foundation models, prompt templates, and configurations for retrieval-augmented generation (RAG).
+Arthur Scope is an API-first enterprise suite of tools for robust, efficient, and scalable metrics, monitoring, 
+dashboards, and alerting.
+Arthur Shield is an API for detecting problems in LLM pipelines - hallucinations, prompt injections, leaks of 
+personally identifiable or otherwise sensitive data, and toxic/malicious language and intent.
+Arthur Bench is an open source python package with built in UI for evaluating and comparing the components of LLM 
+pipelines - different foundation models, prompt templates, and configurations for retrieval-augmented generation (RAG).
 Arthur Chat is a chat application that makes it easy to securely use LLMs with business data."""
     conversation = [{"role": "system", "content": system_prompt}, {"role": "user", "content": prompt}]
     response = command_r_respond(conversation)
     return response
+
+
+def chat():
+    running = True
+    conversation = [{"role": "system", "content": "You are a helpful AI assistant."}]
+    while running:
+        user_input = input(">>>")
+        conversation.append({"role": "user", "content": user_input})
+        response = command_r_respond(conversation, verbose=True)
+        conversation.append({"role": "chatbot", "content": response})
