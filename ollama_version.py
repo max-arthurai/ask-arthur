f15c639d568e29908b23f3a06a245f5428a390ec
Max Cembalest <maxcembalest@Maxs-MBP-2.local.meter>
2024-04-04 13:20:43 -0400

    cohere ollama version working

--- ollama_version.py
+++ ollama_version.py
@@ -3,12 +3,7 @@ import json
 import ollama
 from transformers import AutoTokenizer
 
-# https://docs.cohere.com/docs/prompting-command-r
-
-tokenizer = AutoTokenizer.from_pretrained(
-    "CohereForAI/c4ai-command-r-v01",
-    trust_remote_code=True
-)
+tokenizer = AutoTokenizer.from_pretrained("CohereForAI/c4ai-command-r-v01")
 
 tools = [
   {
@@ -29,90 +24,118 @@ tools = [
   }
 ]
 
-# print(tool_use_prompt)
-# assert 0 == 1
-
-
-def embed(s: str, model='nomic-embed-text'):
-    return ollama.embeddings(model=model, prompt=s)['embedding']
-
-
-def message(s: str, role="USER"):
-    assert role.upper() in ["USER", "CHATBOT", "SYSTEM"]
-    return f"<|START_OF_TURN_TOKEN|><|{role.upper()}_TOKEN|>{s}<|END_OF_TURN_TOKEN|>"
-
-
 chroma_client = chromadb.PersistentClient("chroma/chroma")
-collection = chroma_client.get_collection("arthur_index")
-# test_prompt = 'how do I run LLM evaluations'
-# k = 3
-# result =
-# print(result)
+arthur_index = chroma_client.get_collection("arthur_index")
 
 
-def run(
+def generate(
     prompt: str,
-    llm_name: str = "command-r",
+    model: str = "command-r",
+    start_phrase: str = "Answer: ",
+    stop_phrase: str = "Grounded answer: ",
     temperature: float = 0.0,
-    embedding_name: str = "nomic-embed-text",
-    k: int = 5,
-    verbose: bool = True
+    verbose: bool = True,
 ):
-    conversation = [
-        {"role": "system", "content": """The purpose of your job is to write search queries specifically for the AI company Arthur AI.
-    Arthur AI is the AI performance company. Arthur offers four products (with integrations between the four):
-    Arthur Scope is an API-first enterprise suite of tools for robust, efficient, and scalable metrics, monitoring, dashboards, and alerting.
-    Arthur Shield is an API for detecting problems in LLM pipelines - hallucinations, prompt injections, leaks of personally identifiable or otherwise sensitive data, and toxic/malicious language and intent.
-    Arthur Bench is an open source python package with built in UI for evaluating and comparing the components of LLM pipelines - different foundation models, prompt templates, and configurations for retrieval-augmented generation (RAG).
-    Arthur Chat is a chat application that makes it easy to securely use LLMs with business data."""}
-    ]
+    """Responds with the command-r model via the Ollama local API
 
-    searching = True
-    while searching:
-        conversation.append({"role": "user", "content": prompt})
-        tool_use_prompt = tokenizer.apply_tool_use_template(
+    Args:
+        prompt: str
+        model: str
+        start_phrase: str
+        stop_phrase: str
+        temperature: float
+        verbose: bool
+    Returns:
+        documents: list[{"title": ..., "text": ...}]
+    """
+    if verbose:
+        print(" ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^ ^")
+        print("   Prompt:\n", prompt)
+    generator = ollama.generate(
+        model=model,
+        options={"temperature": temperature},
+        prompt=prompt,
+        stream=True,
+    )
+    response = ""
+    for chunk in generator:
+        chunk_text = chunk["response"]
+        response += chunk_text
+        if verbose:
+            print(chunk_text, end="", flush=True)
+        if stop_phrase in response:
+            response = response.split(stop_phrase)[0]
+            break
+    if start_phrase in response:
+        response = response.split(start_phrase)[1]
+    return response
+
+
+def get_search_documents(s: str, k: int = 3):
+    """Uses the Nomic embedding model via the Ollama local API to search for relevant documents from arthur_index
+
+    Args:
+        s: str, search query
+        k: int, number of search results to return
+    Returns:
+        documents: list[{"title": ..., "text": ...}]
+    """
+    search_result = arthur_index.query(
+        query_embeddings=ollama.embeddings(model="nomic-embed-text", prompt=s)['embedding'],
+        where={"content_type": {"$ne": "arthur_blog"}},
+        n_results=k
+    )
+    documents = [
+        {"title": title, "text": text}
+        for title, text in zip(
+            [x['source'] for x in search_result['metadatas'][0]],
+            search_result['documents'][0]
+        )
+    ]
+    return documents
+
+
+def command_r_respond(conversation: list[dict[str:str]], verbose=True):
+    """Responds to a conversation"""
+    tool_prompt = tokenizer.apply_tool_use_template(
+        conversation,
+        tools=tools,
+        tokenize=False,
+        add_generation_prompt=True,
+    )
+    if verbose: print("choosing tool")
+    tool_response = generate(tool_prompt, verbose=False)
+    tool_response = json.loads(tool_response.split("```json")[1].split("```")[0])
+    tool_name = tool_response[0]["tool_name"]
+    if tool_name == "directly_answer":
+        response_prompt = tokenizer.apply_chat_template(
             conversation,
-            tools=tools,
             tokenize=False,
             add_generation_prompt=True,
         )
-        tool_use_response_generator = ollama.generate(
-            model=llm_name,
-            options={"temperature": temperature},
-            prompt=tool_use_prompt,
-            stream=True,
+    elif tool_name == "internet_search":
+        query = tool_response[0]["parameters"]["query"]
+        documents = get_search_documents(query)
+        response_prompt = tokenizer.apply_grounded_generation_template(
+            conversation,
+            documents=documents,
+            citation_mode="accurate",
+            tokenize=False,
+            add_generation_prompt=True,
         )
-
-        tool_use_response = ""
-        for chunk in tool_use_response_generator:
-            print(chunk)
-            chunk_text = chunk["response"]
-            tool_use_response += chunk_text
-            if verbose:
-                print(chunk_text, end="", flush=True)
-
-        try:
-            # parse response
-            tool_use_response = json.loads(tool_use_response.split("```json")[1].split("```")[0])
-            if len(tool_use_response) == 1:
-                tool_use_response = tool_use_response[0]
-            else:
-                print("Multiple responses!")
-                print(tool_use_response)
-                assert 0==1
-            if tool_use_response["tool_name"] == "internet_search":
-                print("starting search...")
-                print("query:", tool_use_response)
-                query = tool_use_response["parameters"]["query"]
-                search_result = collection.query(query_embeddings=embed(query, model=embedding_name), n_results=k)
-                print(search_result)
-                assert 0==1
-            else:
-                searching = False
-        except Exception as e:
-            print("could not parse response")
-            print(tool_use_response)
-            searching = False
+    else:
+        raise ValueError("bad tool name :/")
+    response = generate(response_prompt, verbose=True)
+    return response
 
 
-run("how does Arthur Bench score summaries")
+def run(prompt: str):
+    system_prompt = """The purpose of your job is to write search queries specifically for the AI company Arthur AI.
+Arthur AI is the AI performance company. Arthur offers four products (with integrations between the four):
+Arthur Scope is an API-first enterprise suite of tools for robust, efficient, and scalable metrics, monitoring, dashboards, and alerting.
+Arthur Shield is an API for detecting problems in LLM pipelines - hallucinations, prompt injections, leaks of personally identifiable or otherwise sensitive data, and toxic/malicious language and intent.
+Arthur Bench is an open source python package with built in UI for evaluating and comparing the components of LLM pipelines - different foundation models, prompt templates, and configurations for retrieval-augmented generation (RAG).
+Arthur Chat is a chat application that makes it easy to securely use LLMs with business data."""
+    conversation = [{"role": "system", "content": system_prompt}, {"role": "user", "content": prompt}]
+    response = command_r_respond(conversation)
+    return response
